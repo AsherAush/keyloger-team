92967b5379b60d97cc5c0cee2b09fd7bf1c92a0d
Shlomo <sh.z.wind@gmail.com>
2025-02-19 13:58:32 +0200

    function to make fil's

--- FileWriter.py
+++ FileWriter.py
@@ -2,6 +2,3 @@ class FileWriter:
     def get_data(self,file_name:str,data:str):
         with open(file_name,"w") as file:
             file.write(data)
-
-a=FileWriter()
-a.get_data("test","shlomo")
