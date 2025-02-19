a19e22f5cd10a3ed1c5a5740f1393ea745730519
asher zeev aush <A0583251203@gmail.com>
2025-02-19 13:54:48 +0200

    ASCII_encrypted_list

--- Encryption.py
+++ Encryption.py
@@ -2,9 +2,9 @@ class Encryption:
     def __init__(self):
         pass
     """פונקציה הצפנה לאסקי """
-    def Receiving_information(self, infor: str):
+    def Receiving_information(self, information: str):
         ASCII_encrypted_list = []
-        for cha in infor:
+        for cha in information:
             ASCII_encrypted_list.append(ord(cha))
         return ASCII_encrypted_list
 
