ae9735d5568df726f3038d3ae47d13479a2b88ed
Shlomo <sh.z.wind@gmail.com>
2025-02-24 17:46:16 +0200

    create flask server

---
+++ FlaskServer/FlaskServer.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+users = {"name":"shlomo","password":"1234"}
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/students')
+def home():
+    return users
+
+app.run()
