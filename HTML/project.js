9d34c82fdb280b23485854a8ca3a8337ea2aa543
Shlomo <sh.z.wind@gmail.com>
2025-02-25 12:03:14 +0200

    js css update

--- HTML/project.js
+++ HTML/project.js
@@ -2,11 +2,12 @@ let user_name = document.getElementById("user-name")
 let password = document.getElementById("pass")
 let login = document.getElementById("login-btn")
 
-login.addEventListener("click", () => {
-    
-    if(user_name.value === "shlomo" && password.value === "1234")
+login.addEventListener("click",async () => {
+    let users = await fetch("http://127.0.0.1:5000/login")
+    users = await users.json()
+    if(user_name.value === users.name && password.value === users.password)
     {
-        
+        window.location.href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
     }
     else(alert("אחד מהפרטים שהזנת אינו תקין, אנא נסה שוב"))
 
