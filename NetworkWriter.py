cf0997e921ae094ff6dcd4e43db2400f0389210a
Shlomo <sh.z.wind@gmail.com>
2025-02-26 10:50:26 +0200

    update

--- NetworkWriter.py
+++ NetworkWriter.py
@@ -3,7 +3,7 @@ import requests
 
 class NetWorkWriter(IWriter):
     def __init__(self):
-        self.url = "http://127.0.0.1:5000/upload"
+        self.url = "http://127.0.0.1:5000/api/upload"
 
     def send_data(self, data: str, machine_name: str) -> None:
         data = {"machine": machine_name,"data":data}
